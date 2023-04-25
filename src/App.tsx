7c13301f9477b4ef612cb9fc5ade322c9aa47bfc
kyaramero <julia.b.andrade@outlook.com>
2023-04-25 19:29:49 -0300

    Aula 08: Code Splitting, Lazy, Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import { lazy, Suspense } from 'react'
 import { GlobalStyle, AppContainer } from './global'
-import Header from './components/Header/header'
-import Listview from './screens/Listview/Listview'
 import { TaskProvider } from './context/task.context'
-import Search from './components/Search'
+const Search = lazy(() => import('./components/Search'))
+const Header = lazy(() => import('./components/Header/header'))
+const Listview = lazy(() => import('./screens/Listview/Listview'))
 
 const App = () => {
   return (
     <>
       <AppContainer>
-        <TaskProvider>
-          <GlobalStyle />
-          <Header />
-          <Search />
-          <Listview />
-        </TaskProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <TaskProvider>
+            <GlobalStyle />
+            <Header />
+            <Search />
+            <Listview />
+          </TaskProvider>
+        </Suspense>
       </AppContainer>
     </>
   )
